345aef15ede94ec14f67873faded1e3dfae49f37
cullenowens <167663888+cullenowens@users.noreply.github.com>
2025-02-08 13:18:59 -0500

    added financial ratios

--- src/Back-End/app.js
+++ src/Back-End/app.js
@@ -5,6 +5,7 @@ const jsonData = loadData();
 if (jsonData) {
     const financialTermsArray = jsonData.financialTerms; //store terms in array
     const financialAnalysisArray = jsonData.financialAnalysis; //store analysis in an array
+    const financialRatiosArray = jsonData.financialRatios; //store ratios in an array
 
     console.log("Financial Terms: ", financialTermsArray);
     console.log("Financial Analysis: ", financialAnalysisArray);
