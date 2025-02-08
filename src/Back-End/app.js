8c78a457b9cf6b3b2ee88691a1ef9685ea69b1ec
cullenowens <167663888+cullenowens@users.noreply.github.com>
2025-02-08 13:50:10 -0500

    hopeCreatingArray

--- src/Back-End/app.js
+++ src/Back-End/app.js
@@ -3,10 +3,12 @@ const loadData = require('./readJson'); //import the read function
 
 const jsonData = loadData();
 if (jsonData) {
-    const financialTermsArray = jsonData.financialTerms; //store terms in array
+    const financialTermsArray = jsonData.financialTerms.map(vocabulary => ({
+        term: vocabulary.term,
+        definition: vocabulary.definition
+    })); //store terms in array
     const financialAnalysisArray = jsonData.financialAnalysis; //store analysis in an array
     const financialRatiosArray = jsonData.financialRatios; //store ratios in an array
 
-    console.log("Financial Terms: ", financialTermsArray);
-    console.log("Financial Analysis: ", financialAnalysisArray);
+    console.log(financialTermsArray);
 }
